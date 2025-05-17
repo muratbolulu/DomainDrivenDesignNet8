8ce8e590ea1799813f2e494cedd94a804bb70968
Murat Bolulu <muratbolulu.business@gmail.com>
2025-05-17 12:14:31 +0300

    benchmark oluşturuldu. kontrol edilecek.

--- DomainDrivenDesignNet8.Domain.ConsoleApp.BenchMarks/Program.cs
+++ DomainDrivenDesignNet8.Domain.ConsoleApp.BenchMarks/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BenchmarkDotNet.Running;
+using DomainDrivenDesignNet8.ConsoleApp.BenchMarks;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //benchmark yaparken release modda çalıştırmayı unutma
+        BenchmarkRunner.Run<BenchMarkService>();
+
+        Console.ReadLine();
+    }
+}
+
+
+public abstract class Entity : IEquatable<Entity>
+{
+    public Guid Id { get; init; }
+
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    //from obj equals. daha yavaş IEquatable a göre
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        if (obj is not Entity entity)
+        {
+            return false;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return entity.Id == Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    //from IEquatable equals 
+    public bool Equals(Entity? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (other is not Entity entity)
+        {
+            return false;
+        }
+
+        if (other.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return entity.Id == Id;
+    }
+}
